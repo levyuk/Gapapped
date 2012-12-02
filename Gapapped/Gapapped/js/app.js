f6d9ac956558499b90db1dc7351a1c08138f569d
Jon <jonathan.levy@hotmail.com>
2012-12-02 13:37:34 +0000

    .

---
+++ Gapapped/Gapapped/js/app.js
@@ -0,0 +1,7 @@
+﻿var app = angular.module('app', ['ngSanitize']);
+
+app.config(['$routeProvider', '$httpProvider', function ($routeProvider, $httpProvider) {
+    $routeProvider.when('/home/', { templateUrl: 'partials/home.html', controller: HomeCtrl });
+    $routeProvider.when('/contact/', { templateUrl: 'partials/contact.html', controller: ContactCtrl });
+    $routeProvider.otherwise({ redirectTo: '/home/' });
+}]);
---
+++ Gapapped/Gapapped/js/controller.js
@@ -0,0 +1,10 @@
+﻿function HomeCtrl($http) {
+
+}
+
+function ContactCtrl($http) {
+
+}
+
+HomeCtrl.$inject = ['$http'];
+ContactCtrl.$inject = ['$http'];
