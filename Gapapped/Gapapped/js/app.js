9c675df2ae0cd08f115c673651255d9ac2b531a0
Jon <jonathan.levy@hotmail.com>
2012-12-02 14:31:00 +0000

    Some new pages

--- Gapapped/Gapapped/js/app.js
+++ Gapapped/Gapapped/js/app.js
@@ -1,7 +1,10 @@
-﻿var app = angular.module('app', ['ngSanitize']);
-
-app.config(['$routeProvider', '$httpProvider', function ($routeProvider, $httpProvider) {
+﻿angular.module('app', []).config(['$routeProvider', '$httpProvider', function ($routeProvider, $httpProvider) {
     $routeProvider.when('/home/', { templateUrl: 'partials/home.html', controller: HomeCtrl });
     $routeProvider.when('/contact/', { templateUrl: 'partials/contact.html', controller: ContactCtrl });
+    $routeProvider.when('/notes/', { templateUrl: 'partials/notes.html', controller: NotesCtrl });
     $routeProvider.otherwise({ redirectTo: '/home/' });
-}]);
+}]).filter('formattedDate', function (dateFilter) {
+    return function (input, format) {
+        return dateFilter(new Date(input), format);
+    }
+});
--- Gapapped/Gapapped/js/controller.js
+++ Gapapped/Gapapped/js/controller.js
@@ -6,5 +6,20 @@ function ContactCtrl($http) {
 
 }
 
+function NotesCtrl($scope, $http) {
+    $scope.notes = [{
+        description: 'Note app running, add more by using the above textbox',
+        dateTime: new Date()
+    }];
+
+    $scope.addNote = function () {
+        $scope.notes.push({
+            description: $scope.newNote,
+            dateTime: new Date()
+        });
+    }
+}
+
 HomeCtrl.$inject = ['$http'];
 ContactCtrl.$inject = ['$http'];
+NotesCtrl.$inject = ['$scope', '$http'];
